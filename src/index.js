064ae43d70483d43675d0a101b727cb159b2cb19
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-06-25 21:44:44 +0300

    first-settings

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env.DB_URI);
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
---
+++ src/server.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+// import pino from 'pino-http';
+import cookieParser from 'cookie-parser';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: '',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }),
+);
+
+app.use(cookieParser());
+app.use('/uploads', express.static('uploads'));
+// app.use(router);
+
+//   app.use(
+//     pino({
+//       transport: {
+//         target: 'pino-pretty',
+//       },
+//     }),
+//   );
+// закоментовано для розробки
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Start page!',
+  });
+});
+
+app.use('*', notFoundHandler);
+
+app.use(errorHandler);
+
+const setupServer = () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+export default setupServer;
