9608a044427e18fe046f19943d9ba4267a197500
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-06-26 18:04:18 +0300

    multer-filename

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -4,7 +4,8 @@ import path from 'path';
 const storage = multer.diskStorage({
   destination: 'uploads/',
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
   },
 });
 
