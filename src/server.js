27f6a0736b96c8730835644ddd370be109c8c7ac
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-06-27 17:38:09 +0300

    local

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: 'https://apartments-delta.vercel.app',
+    // origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
